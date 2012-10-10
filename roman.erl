44e8ddc41d2d5f24e4e22512c10e84827359e817
Brian H. Ward <glyphrider@gmail.com>
2012-10-09 21:54:21 -0400

    significant modification of alorith (now with data-driven)

--- roman.erl
+++ roman.erl
@@ -5,38 +5,22 @@
 -export([to/1]).
 -export([from/1]).
 
-to(N) -> to_roman(N,"").
-from(S) -> from_roman(S,0).
+-define(XLATE,[{1,"I"},{4,"IV"},{5,"V"},{9,"IX"},{10,"X"},{40,"XL"},{50,"L"},{90,"XC"},{100,"C"},{400,"CD"},{500,"D"},{900,"CM"},{1000,"M"}]).
 
-to_roman(N,S) when N >= 1000 -> to_roman(N-1000,S ++ "M");
-to_roman(N,S) when N >= 900 -> to_roman(N-900,S ++ "CM");
-to_roman(N,S) when N >= 500 -> to_roman(N-500,S ++ "D");
-to_roman(N,S) when N >= 400 -> to_roman(N-400,S ++ "CD");
-to_roman(N,S) when N >= 100 -> to_roman(N-100,S ++ "C");
-to_roman(N,S) when N >= 90 -> to_roman(N-90,S ++ "XC");
-to_roman(N,S) when N >= 50 -> to_roman(N-50,S ++ "L");
-to_roman(N,S) when N >= 40 -> to_roman(N-40,S ++ "XL");
-to_roman(N,S) when N >= 10 -> to_roman(N-10,S ++ "X");
-to_roman(N,S) when N >= 9 -> to_roman(N-9,S ++ "IX");
-to_roman(N,S) when N >= 5 -> to_roman(N-5,S ++ "V");
-to_roman(N,S) when N >= 4 -> to_roman(N-4,S ++ "IV");
-to_roman(N,S) when N >= 1 -> to_roman(N-1,S ++ "I");
-to_roman(0,S) -> S.
+to(N) -> to(N,"",lists:reverse(?XLATE)).
+to(0,S,_) -> S;
+to(N,S,[H|T]) -> to(N,S,H,T).
+to(N,S,{A,R},T) when N >= A -> to(N-A,S ++ R, {A,R},T);
+to(N,S,_,T) -> to(N,S,T). 
 
-from_roman("M" ++ S,N) -> from_roman(S,N+1000);
-from_roman("CM" ++ S,N) -> from_roman(S,N+900);
-from_roman("D" ++ S,N) -> from_roman(S,N+500);
-from_roman("CD" ++ S,N) -> from_roman(S,N+400);
-from_roman("C" ++ S,N) -> from_roman(S,N+100);
-from_roman("XC" ++ S,N) -> from_roman(S,N+90);
-from_roman("L" ++ S,N) -> from_roman(S,N+50);
-from_roman("XL" ++ S,N) -> from_roman(S,N+40);
-from_roman("X" ++ S,N) -> from_roman(S,N+10);
-from_roman("IX" ++ S,N) -> from_roman(S,N+9);
-from_roman("V" ++ S,N) -> from_roman(S,N+5);
-from_roman("IV" ++ S,N) -> from_roman(S,N+4);
-from_roman("I" ++ S,N) -> from_roman(S,N+1);
-from_roman("",N) -> N.
+from(S) -> from(S,0,lists:reverse(?XLATE)).
+from("",N,_) -> N;
+from(S,N,[H|T]) -> from(S,N,H,T).
+from(S,N,{A,R},T) ->
+	case string:str(S,R) of
+		1 -> from(string:substr(S,string:len(R)+1),N+A,{A,R},T);
+		_ -> from(S,N,T)
+	end.
 
 -ifdef(EUNIT).
 to_roman_one_test() -> "I" = to(1).
