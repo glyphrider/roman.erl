36ad11d06652dbdb0a6b9f28830cc1cb825b7bd2
Brian H. Ward <glyphrider@gmail.com>
2020-09-21 13:06:34 -0400

    update tests to make js version

--- roman.erl
+++ roman.erl
@@ -33,32 +33,60 @@ from(String,Number,[{Arabic,Roman}|Tail]=List) ->
 -ifdef(EUNIT).
 
 -define(TEST_CASES,[
-		    {"I",1},
-		    {"II",2},
-		    {"III",3},
-		    {"IV",4},
-		    {"V",5},
-		    {"VI",6},
-		    {"IX",9},
-		    {"X",10},
-		    {"XVI",16},
-		    {"XIX",19},
-		    {"XXVIII",28},
-		    {"XL",40},
-		    {"L",50},
-		    {"XC",90},
-		    {"C",100},
-		    {"D",500},
-		    {"CD",400},
-		    {"M",1000},
-		    {"CM",900},
-		    {"MCMLXVIII",1968},
-		    {"LXIX",69}
-		   ]).
+                    {0, ""},
+		    {1, "I"},
+		    {2, "II"},
+		    {3, "III"},
+		    {4, "IV"},
+		    {5, "V"},
+		    {6, "VI"},
+		    {7, "VII"},
+		    {8, "VIII"},
+		    {9, "IX"},
+		    {10, "X"},
+		    {11, "XI"},
+		    {14, "XIV"},
+		    {15, "XV"},
+		    {16, "XVI"},
+		    {19, "XIX"},
+		    {20, "XX"},
+		    {21, "XXI"},
+		    {39, "XXXIX"},
+		    {40, "XL"},
+		    {41, "XLI"},
+		    {42, "XLII"},
+		    {43, "XLIII"},
+		    {44, "XLIV"},
+		    {45, "XLV"},
+		    {46, "XLVI"},
+		    {47, "XLVII"},
+		    {48, "XLVIII"},
+		    {49, "XLIX"},
+		    {50, "L"},
+		    {89, "LXXXIX"},
+		    {90, "XC"},
+		    {91, "XCI"},
+		    {99, "XCIX"},
+		    {100, "C"},
+		    {101, "CI"},
+		    {200, "CC"},
+		    {399, "CCCXCIX"},
+		    {400, "CD"},
+		    {401, "CDI"},
+		    {499, "CDXCIX"},
+		    {500, "D"},
+		    {501, "DI"},
+		    {899, "DCCCXCIX"},
+		    {900, "CM"},
+		    {999, "CMXCIX"},
+		    {1000, "M"},
+		    {1001, "MI"},
+		    {1968, "MCMLXVIII"}
+		    ]).
 
 roman_to_arabic_test_() ->
     {"Roman to Arabic",
-    lists:map(fun({R,A}) ->
+    lists:map(fun({A,R}) ->
 		      {lists:flatten([R," to ",integer_to_list(A)]),
 		       ?_assertEqual(A,from(R))}
 	      end,
@@ -66,7 +94,7 @@ roman_to_arabic_test_() ->
 
 arabic_to_roman_test_() ->
     {"Arabic to Roman",
-     lists:map(fun({R,A}) ->
+     lists:map(fun({A,R}) ->
 		       {lists:flatten([integer_to_list(A)," to ",R]),
 			?_assertEqual(R,to(A))}
 	       end,
